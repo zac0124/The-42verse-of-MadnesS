3e429d070aeb2b399fcf3a8e17d0da241ccffc95
Zac0124 <mzorigt91@gmail.com>
2022-06-27 14:29:01 +0930

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zmunkhja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 12:06:49 by zmunkhja          #+#    #+#             */
-/*   Updated: 2022/06/27 14:08:22 by zmunkhja         ###   ########.fr       */
+/*   Updated: 2022/06/27 14:28:19 by zmunkhja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ static	char	*find_new_line(int fd, char *buf, char *backup)
 	char	*temp;
 
 	line = 1;
-	while(line != '\0')
+	while (line != '\0')
 	{
 		line = read(fd, buf, BUFFER_SIZE);
-		if(line < 0)
+		if (line < 0)
 			return (0);
-		else if(line == 0 )
-			break;
+		else if (line == 0)
+			break ;
 		buf[line] = '\0';
-		if(!backup)
+		if (!backup)
 			backup = ft_strdup("");
 		temp = backup;
 		backup = ft_strjoin(temp, buf);
 		free(temp);
 		temp = 0;
-		if(ft_strchr(buf,'\n'))
-			break;
+		if (ft_strchr (buf, '\n'))
+			break ;
 	}
 	return (backup);
 }
@@ -42,14 +42,14 @@ static	char	*extract(char *line)
 {
 	size_t	i;
 	char	*backup;
-	
+
 	i = 0;
-	while(line[i] != '\n' && line[i] != '\0')
+	while (line[i] != '\n' && line[i] != '\0')
 		i++;
-	if(line[i] == '\0' || line[i] != '\0')
+	if (line[i] == '\0' || line[i] != '\0')
 		return (0);
 	backup = ft_substr(line, i + 1, ft_strlen(line) - i);
-	if(*backup == '\0')
+	if (*backup == '\0')
 	{
 		free(backup);
 		backup = 0;
@@ -64,16 +64,16 @@ char	*get_next_line(int fd)
 	char	*buf;
 	static char	*backup;
 
-	if(fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	buf = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if(!buf)
+	buf = (char*) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
 		return (0);
 	line = find_new_line(fd, buf, backup);
 	free(buf);
 	buf = 0;
-	if(!line)
+	if (!line)
 		return (0);
 	backup = extract(line);
-	return	(line);
+	return (line);
 }
